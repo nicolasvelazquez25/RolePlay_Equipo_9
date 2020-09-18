85dfcb1da14ab7aab1c7a04e176e6bcdf21475de
agusbenia25 <agusbenia@hotmail.com>
2020-09-18 15:47:29 -0300

    Class Witch

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,11 +8,18 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Spell crucio = new Spell("Crucio", 50, 20);
-            Spellbook libro_hechizo = new Spellbook("Libro hechizos", crucio);
-            WizardStick palo = new WizardStick("Palo magico", 60, 0);
-            Wizard merlin = new Wizard("Merlin", 100, libro_hechizo, palo);
-            Console.WriteLine(libro_hechizo.Attack);
+            //Parte de Agustina Benia
+            Dagger dagger = new Dagger("Dagger", 88, 34);
+            IronNails ironNails = new IronNails("Iron Nails", 34, 78);
+            Witch witch1 = new Witch("Manon",100, dagger, ironNails);
+            Witch witch2 = new Witch("Asterin", 100, dagger, ironNails);
+            Console.WriteLine(witch1.MessageCharacter());
+            Console.WriteLine(witch2.MessageCharacter());
+            Console.WriteLine(witch2.MessageHealth());
+            witch1.MakeDmg(witch2);
+            Console.WriteLine(witch2.MessageHealth());
+            witch2.RestoreHealth();
+            Console.WriteLine(witch2.MessageHealth());
         }
     }
 }
